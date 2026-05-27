3d5e17719d04ee99ce76cbab4b109308b34d5d1f
codingwithriya <itisghoshriya@gmail.com>
2026-05-28 00:25:34 +0530

    Day 2 and 3 important concepts

---
+++ src/Anonymous.java
@@ -0,0 +1,14 @@
+class Ano{
+    public void show(){
+        System.out.println("in Show of Ano");
+    }
+}
+
+public class Anonymus {
+    public static void main(String[] args) {
+        Ano obj = new Ano()
+        {
+            
+        };
+    }
+}
---
+++ src/Riya/CollectionsTest.java
@@ -0,0 +1,4 @@
+package Riya;
+
+public class CollectionsTest {
+}
